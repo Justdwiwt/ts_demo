6ae462db06da2c7563bb90e6c6746768ac257d3c
Justdwiwt <len_master@hotmail.com>
2018-11-24 16:19:41 +0800

    update demo

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 "use strict";
 var a = 10;
 console.log(a);
+var i = 1;
+console.log(i++ + ++i);
+var j = 1;
+console.log(j++ + j++ + j++);
+var k = 1;
+console.log(++k + ++k + ++k);
--- test.ts
+++ test.ts
@@ -1,2 +1,11 @@
-var a:number = 10;
+var a: number = 10;
 console.log(a);
+
+let i = 1;
+console.log(i++ + ++i);
+
+let j = 1;
+console.log(j++ + j++ + j++);
+
+let k = 1;
+console.log(++k + ++k + ++k);
